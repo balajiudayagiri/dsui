21b43c8926c06459b3a133895d61bf0272378f0c
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-05-18 03:45:24 +0530

    updated the scroll animtion

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { CodeEditor } from "./CodeEditor";
 export { ErrorBoundary } from "./ErrorBoundary";
+export { ScrollAnimationWrapper } from "./ScrollAnimationWrapper";
 // hooks
 export { usePersistentState } from "./hooks/usePersistentState";
